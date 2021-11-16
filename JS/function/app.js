16eb17efbefaa8264237d2c0e6d1ad4bed9dc92d
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-11-16 12:04:12 +0200

    ss
    ss

---
+++ JS/function/app.js
@@ -0,0 +1,16 @@
+console.log('hello');
+const  getSquared = num => Number(num) ** 2;
+
+console.log(getSquared('45'))
+console.log(getSquared(5))
+console.log(getSquared('im number'))
+
+//const res = getSenseOfLife();
+//console.log(res);
+
+console.log(getSquared("2"));
+const printMessage = (num) =>  'I am ' + num + ' years old';
+
+console.log(printMessage(34))
+
+const mult = (num1, num2) => Number(num1) + Number(num2);
